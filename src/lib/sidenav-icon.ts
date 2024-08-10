d6f9ed3488fa56422ad74af539dbbd0d76e8ce2c
Raghav Jindal <raghavjindal0212@gmail.com>
2024-08-10 13:03:44 +0530

    added raffle screen

--- src/lib/sidenav-icon.ts
+++ src/lib/sidenav-icon.ts
@@ -8,7 +8,8 @@ export const sideNavIcons = [
     href: "/roulette"
   },
   {
-    icons: "/svg/side-icon-3.svg"
+    icons: "/svg/side-icon-3.svg",
+    href: "/raffle"
   },
   {
     icons: "/svg/side-icon-4.svg"
