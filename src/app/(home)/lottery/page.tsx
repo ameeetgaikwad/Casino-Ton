4251f14f808ddba8e0d76896c86e658853f02d79
Vatsal Sanchala <bevatsal1122@gmail.com>
2024-08-20 16:33:24 +0530

    feat: USDT token on BNB testnet

--- src/app/(home)/lottery/page.tsx
+++ src/app/(home)/lottery/page.tsx
@@ -7,7 +7,496 @@ import { useContract } from "@/hooks/use-contract";
 import { MyLotteries } from "@/components/my-lotteries";
 import { Button } from "@/components/ui/button";
 import { tickets } from "@/lib/db/schema/tickets";
-import { Switch } from "@/components/ui/switch"
+import { Switch } from "@/components/ui/switch";
+import { Web3Provider } from "ethers/providers";
+import { useCallback } from "react";
+import { ethers } from "ethers";
+import { useAccount } from "wagmi";
+import { fr } from "ethers/wordlists";
+
+const USDTabi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "allowance",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "needed",
+        type: "uint256",
+      },
+    ],
+    name: "ERC20InsufficientAllowance",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "balance",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "needed",
+        type: "uint256",
+      },
+    ],
+    name: "ERC20InsufficientBalance",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "approver",
+        type: "address",
+      },
+    ],
+    name: "ERC20InvalidApprover",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "receiver",
+        type: "address",
+      },
+    ],
+    name: "ERC20InvalidReceiver",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+    ],
+    name: "ERC20InvalidSender",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+    ],
+    name: "ERC20InvalidSpender",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "EnforcedPause",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "ExpectedPause",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "OwnableInvalidOwner",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "OwnableUnauthorizedAccount",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "Paused",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "Unpaused",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+    ],
+    name: "allowance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "burn",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "burnFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pause",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "paused",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "unpause",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
 type Raffle = {
   lotteryId: number;
@@ -53,19 +542,40 @@ const TicketProgressBar = ({ ticketsSold, totalTickets }) => {
 };
 
 const RaffleGame = () => {
-
   const [myLotteries, setMyLotteries] = useState<MyLotteriesProps[] | []>([]);
   const [trendingRaffles, setTrendingRaffles] = useState<Raffle[]>([]);
   const [selectedLotteryId, setSelectedLotteryId] = useState<number>();
-
   const { smartContract, error, address } = useContract("LOTTERY");
-
   const [megaRaffle, setMegaRaffle] = useState<Raffle>();
 
   const changeMegaRaffle = (raffle: Raffle) => {
     setSelectedLotteryId(raffle.lotteryId);
     setMegaRaffle(raffle);
   };
+
+  const [USDTcontract, setUSDTcontract] = useState<any>();
+
+  const loadBlockchainData = useCallback(async () => {
+    const ethereum = (window as any).ethereum;
+    var provider: Web3Provider | null = null;
+    if (ethereum) {
+      await ethereum.enable();
+      provider = new ethers.providers.Web3Provider(ethereum);
+    }
+    if (!provider) return;
+
+    setUSDTcontract(
+      new ethers.Contract(
+        "0xD3b2530fDF4b887061cAeeea9F6f257d2cdA00ae",
+        USDTabi,
+        provider.getSigner()
+      )
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(USDTcontract);
+  }, [USDTcontract]);
 
   const initialLoadd = async () => {
     try {
@@ -82,12 +592,9 @@ const RaffleGame = () => {
       let tx = await smartContract?.getActiveLotteries();
       let activeLotteries: any[] = [];
 
-      let bestLottery : Raffle = {} as Raffle;
-
+      let bestLottery: Raffle = {} as Raffle;
 
       for (let i = 0; i < tx.length; i++) {
-        
-
         const thisLottery = {
           lotteryId: tx[i][0],
           amount: parseInt(tx[i][2]._hex, 16),
@@ -99,14 +606,20 @@ const RaffleGame = () => {
 
         activeLotteries.push(thisLottery);
 
-        if (!bestLottery.prizePool || (bestLottery.totalTickets - bestLottery.ticketsSold > thisLottery.totalTickets - thisLottery.ticketsSold)) {
+        if (
+          !bestLottery.prizePool ||
+          bestLottery.totalTickets - bestLottery.ticketsSold >
+            thisLottery.totalTickets - thisLottery.ticketsSold
+        ) {
           bestLottery = thisLottery;
         }
       }
       console.log("LALLA", bestLottery);
       setTrendingRaffles(activeLotteries);
-      setMegaRaffle(bestLottery);
-      setSelectedLotteryId(bestLottery.lotteryId);
+      if (!selectedLotteryId) {
+        setMegaRaffle(bestLottery);
+        setSelectedLotteryId(bestLottery.lotteryId);
+      }
 
       tx = await smartContract?.getPlayerLotteries(address);
       let myLotteriesData: any[] = [];
@@ -124,8 +637,6 @@ const RaffleGame = () => {
       }
 
       setMyLotteries(myLotteriesData);
-
-
     } catch (err: any) {
       console.log(err);
     }
@@ -142,8 +653,24 @@ const RaffleGame = () => {
       if (!smartContract) {
         throw new Error(`Contract not initialized`);
       }
-      const tx = await smartContract?.buyTicket();
+      console.log("HAHHAHAHAA");
+
+      var totalCost = megaRaffle!.amount * 1;
+      console.log(totalCost);
+
+      console.log(USDTcontract);
+
+      const approval = await USDTcontract.approve(
+        smartContract.address,
+        totalCost
+      );
+      console.log(approval);
+      // now finl transaction with value totalCost
+      const tx = await smartContract?.buyTickets(lotteryId, 1, {
+        value: totalCost,
+      });
       console.log(tx);
+      initialLoadd();
     } catch (err: any) {
       console.log(err);
     }
@@ -155,10 +682,10 @@ const RaffleGame = () => {
     }
 
     try {
+      loadBlockchainData();
       initialLoadd();
     } catch (err: any) {
       console.log(err);
-
     }
   }, [smartContract]);
 
@@ -183,15 +710,15 @@ const RaffleGame = () => {
       </div>
 
       <div className="flex flex-col">
-
         <div className="w-full h-full mt-10 items-center justify-center align-middle mx-auto flex flex-col md:flex-row md:items-start md:space-x-8">
-          {megaRaffle ? 
+          {megaRaffle ? (
             <div className="md:w-[500px] bg-shade p-4 rounded-md p-6 max-w-[500px]">
               <h2 className="text-5xl sm:text-5xl font-bold mb-4 text-yellow-400">
                 ${megaRaffle.prizePool}
               </h2>
               <p className="mb-4 text-sm text-gray-400">
-                Lottery is drawn once all the {megaRaffle.totalTickets} tickets have been sold
+                Lottery is drawn once all the {megaRaffle.totalTickets} tickets
+                have been sold
               </p>
 
               <TicketProgressBar
@@ -199,20 +726,24 @@ const RaffleGame = () => {
                 totalTickets={megaRaffle.totalTickets}
               />
               <Button
-                
+                onClick={() => onBuyTicket(megaRaffle.lotteryId)}
                 className="font-heading my-6 text-xl w-full"
               >
                 Buy Ticket
               </Button>
 
-              <p className="text-sm text-gray-400">Ticket prize: {megaRaffle.amount}</p>
+              <p className="text-sm text-gray-400">
+                Ticket prize: {megaRaffle.amount}
+              </p>
             </div>
-          : <> </> }
+          ) : (
+            <> </>
+          )}
           {/* Trending Raffles Section - Right Side */}
           <div className="md:w-7/10 bg-shade p-5 rounded-md">
             <h3 className="text-3xl font-bold mb-2 flex items-center">
-              <Flame className="text-orange-500 mr-2 h-8 w-8" /> Trending Lottery
-              Draws
+              <Flame className="text-orange-500 mr-2 h-8 w-8" /> Trending
+              Lottery Draws
             </h3>
             <p className="mb-4 text-lg text-gray-400">
               Lottery is drawn once target slots have been sold
@@ -226,12 +757,11 @@ const RaffleGame = () => {
                   } border border-yellow-400`}
                 >
                   <div className="flex justify-between">
-                  <p className="text-lg font-bold mb-1">
-                    ${raffle.amount.toLocaleString()}
-                    <br />
-                   
-                  </p>
-                  <Switch
+                    <p className="text-lg font-bold mb-1">
+                      ${raffle.amount.toLocaleString()}
+                      <br />
+                    </p>
+                    <Switch
                       checked={raffle.lotteryId === megaRaffle?.lotteryId}
                       onCheckedChange={() => changeMegaRaffle(raffle)}
                     />
@@ -242,7 +772,7 @@ const RaffleGame = () => {
                   <p className="text-s text-gray-400 mb-2">
                     Tickets: {raffle.ticketsSold} of {raffle.totalTickets}
                   </p>
-                  
+
                   {/* {raffle && (
                     <button
                       onClick={() => buyTrendingRaffleTicket(index)}
